9af1c862229d1bc01bab389dbf8b0a7728594f2c
Luiz Oliveira <felipeluiz75@outlook.com>
2026-06-20 13:18:13 -0300

    fix: vercel.json

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: Number(import.meta.env.VITE_API_URL_TIMEOUT) || 5000,
+  timeout: 30000,
   withCredentials: true,
 });
 
